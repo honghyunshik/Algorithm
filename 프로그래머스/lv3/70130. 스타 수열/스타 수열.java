7f932c599bb02f64a592889059fd6baff3cd340c
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-05-11 17:54:09 +0900

    [level 3] Title: 스타 수열, Time: 0.03 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/70130. 스타 수열/스타 수열.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] a) {
+        int answer = 0;
+        int[] count = new int[a.length];
+        for(int num:a){
+            count[num]++;
+        }
+        
+   for(int i=0;i<a.length;i++){
+       
+       if(count[i]==0) continue;
+       if(count[i]<=answer) continue;
+       int sum = 0;
+       
+       for(int j=0;j<a.length-1;j++){
+           
+           if(a[j]!=i&&a[j+1]!=i) continue;
+           if(a[j]==a[j+1]) continue;
+           sum++;
+           j++;
+       }
+       answer = Math.max(answer,sum);
+   }
+        return answer*2;
+    }
+}
