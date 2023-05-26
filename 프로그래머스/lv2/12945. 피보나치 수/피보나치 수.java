e28409dcc78260b9d60242fad06bb7d56dd7d96b
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-05-26 16:09:50 +0900

    [level 2] Title: 피보나치 수, Time: 2.58 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        
+        if(n<2) return n;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            
+            dp[i] = (dp[i-1] + dp[i-2])%1234567;
+        }
+        
+        return dp[n];
+    }
+}
