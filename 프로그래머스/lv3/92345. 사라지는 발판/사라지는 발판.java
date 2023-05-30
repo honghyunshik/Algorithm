c032a2d9897ebb3a4e73becb90a3a85909e6ed03
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-05-31 01:47:10 +0900

    [level 3] Title: 사라지는 발판, Time: 0.18 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/lv3/92345. 사라지는 발판/사라지는 발판.java
@@ -1,59 +1,61 @@
+import java.util.Arrays;
+
 class Solution {
-    
-    int[][] BOARD;
-    int[][] D = {{0,1},{0,-1},{1,0},{-1,0}};
-    int N,M;
-    
+
+    static int[][] BOARD;
+    static boolean[][] VISITED;
+    static int[][] D = {{0,1},{0,-1},{1,0},{-1,0}};
+    static int L,R;
+
+
     public int solution(int[][] board, int[] aloc, int[] bloc) {
-        
-        int answer = -1;
+
         BOARD = board;
-        N = board.length;
-        M = board[0].length;
-        
-        return dfs(aloc[0],aloc[1],bloc[0],bloc[1],0).cnt;
+        L = board.length;
+        R = board[0].length;
+        VISITED = new boolean[L][R];
+
+        return dfs(aloc[0],aloc[1],bloc[0],bloc[1]).dis;
     }
-    
-    private Result dfs(int x1, int y1, int x2, int y2, int depth){
+
+    //이길 수 있는 방법이 있으면 무조건 이기고
+    //질 수 밖에 없다면 최대한 높은 숫자를 반환
+    private Result dfs(int x1, int y1,int x2, int y2){
+
+        if(BOARD[x1][y1]==0) return new Result(false,0);
         
+        BOARD[x1][y1] = 0;
         boolean win = false;
-        int max = depth;
-        int min = 5*5;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         
-        if(BOARD[x1][y1]==1){
-            
-            for(int i=0;i<4;i++){
-                
-                int nextX1 = x1 + D[i][0];
-                int nextY1 = y1 + D[i][1];
-                if(nextX1<0||nextX1>N-1||nextY1<0||nextY1>M-1) continue;
-                if(BOARD[nextX1][nextY1]==0) continue;
-                BOARD[x1][y1] = 0;
-                Result result = dfs(x2,y2,nextX1,nextY1,depth+1);
-                win |= !result.win;
-                //x2, y2가 이긴거니까 x1, y1은 패배
-                if(result.win){
-                    max = Math.max(max,result.cnt);
-                }else{
-                    min = Math.min(min,result.cnt);
-                }
-                BOARD[x1][y1] = 1;
-                
-            }
-            
+        
+        for(int i=0;i<4;i++){
+
+            int nextX1 = x1 + D[i][0];
+            int nextY1 = y1 + D[i][1];
+            if(nextX1<0||nextX1>L-1||nextY1<0||nextY1>R-1
+                    ||BOARD[nextX1][nextY1]==0) continue;
+            Result re = dfs(x2,y2,nextX1,nextY1);
+            if(!re.win){
+                win = true;
+                min = Math.min(re.dis,min);
+            }else max = Math.max(re.dis,max);
         }
-        return new Result(win,win?min:max); 
+        BOARD[x1][y1] = 1;
+        
+        if(!win&&max==Integer.MIN_VALUE) return new Result(false,0);
+        return new Result(win,win?min+1:max+1);
         
     }
-    
+
 }
 
 class Result{
-    
     boolean win;
-    int cnt;
-    Result(boolean win, int cnt){
+    int dis;
+    Result(boolean win, int dis){
         this.win = win;
-        this.cnt = cnt;
+        this.dis = dis;
     }
 }
