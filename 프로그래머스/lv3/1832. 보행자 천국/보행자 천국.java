0cef4fd78741b076d27733d9ee014b87880f7aa1
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-02 21:58:10 +0900

    [level 3] Title: 보행자 천국, Time: 137.37 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/lv3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/lv3/1832. 보행자 천국/보행자 천국.java
@@ -1,53 +1,50 @@
-import java.util.*;
 class Solution {
     
     int MOD = 20170805;
-    static int[][] CITY;
-    static int[][] dp;
-    static int[][] D = {{0,1},{1,0}};
-    static int M,N;
-    //자동차는 밑이나 오른쪽으로 이동한다
+    int[][] CITYMAP;
+    int[][][] dp;
+    int M,N;
+    
     public int solution(int m, int n, int[][] cityMap) {
         int answer = 0;
-        dp = new int[m][n];
-        M=m; N=n;
-        for(int i=0;i<n;i++){
-            if(cityMap[0][i]==1) break;
-            if(cityMap[0][i]==2) continue;
-            dp[0][i] = 1;
-        }
-        for(int i=0;i<m;i++){
-            if(cityMap[i][0]==1) break;
-            if(cityMap[i][0]==2) continue;
-            dp[i][0] = 1;
+        CITYMAP = cityMap;
+        M = m;
+        N = n;
+        //0은 ->  1은 아래
+        dp = new int[M][N][2];
+        
+        //맨 위
+        for(int i=1;i<N;i++){
+            if(CITYMAP[0][i]==1) break;
+            dp[0][i][0] = 1;
+            if(CITYMAP[0][i]==0) dp[0][i][1] = 1;
         }
         
-        for(int i=1;i<m;i++){
+        //맨 왼쪽
+        for(int i=1;i<M;i++){
+            if(CITYMAP[i][0]==1) break;
+            dp[i][0][1] = 1;
+            if(CITYMAP[i][0]==0) dp[i][0][0] = 1;
+        }
+        
+        for(int i=1;i<M;i++){
             
-            for(int j=1;j<n;j++){
+            for(int j=1;j<N;j++){
                 
-                if(cityMap[i][j]==1) continue;
-                if(cityMap[i][j]==2){
-                    int p = i+1;
-                    while(dp[i-1][j]!=0&&p<m&&cityMap[p][j]==2){
-                        p++;
-                    }
-                    if(p<m&&cityMap[p][j]==0) dp[p][j] += dp[i-1][j];
-                    int q = j+1;
-                    while(dp[i][j-1]!=0&&q<n&&cityMap[i][q]==2){
-                        q++;
-                    }
-                    if(q<n&&cityMap[i][q]==0) dp[i][q] += dp[i][j-1];
-                    
+                if(CITYMAP[i][j]==1) continue;
+                else if(CITYMAP[i][j]==2){
+                    dp[i][j][0] += dp[i][j-1][0];
+                    dp[i][j][1] += dp[i-1][j][1];
                 }else{
-                    dp[i][j]+=dp[i-1][j] + dp[i][j-1];
+                    dp[i][j][0] += dp[i-1][j][1] + dp[i][j-1][0];
+                    dp[i][j][1] += dp[i-1][j][1] + dp[i][j-1][0];
                 }
-                dp[i][j]%= 20170805;
+                dp[i][j][0] %= MOD;
+                dp[i][j][1] %= MOD;
             }
-            
         }
         
-        return dp[m-1][n-1];
+        
+        return (dp[M-1][N-2][0] + dp[M-2][N-1][1])%MOD;
     }
-    
 }
