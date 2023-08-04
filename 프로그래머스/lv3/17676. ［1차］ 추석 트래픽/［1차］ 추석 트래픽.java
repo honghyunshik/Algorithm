d8f4f3dca433f20361344d46d4955619ec9b76dd
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-04 15:43:32 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 0.13 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/lv3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -15,7 +15,7 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             int cnt = 0;
             for(int j=i;j<arr.length;j++){   
-                if(arr[i][1]+1000>arr[j][0]&&arr[j][1]>=arr[i][1]){
+                if(arr[i][1]+1000>arr[j][0]){
                     cnt++;
                 }
             }
