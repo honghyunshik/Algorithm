b376b9320a679d40003c714b43fd06eb87b0fc82
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-04 18:09:19 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 0.68 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/lv3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -1,53 +1,40 @@
 import java.util.*;
+
 class Solution {
+    
     public int solution(String[] lines) {
+        int answer = 1;
         
-        int answer = 0;
         int[][] arr = new int[lines.length][2];
-        
         for(int i=0;i<lines.length;i++){
-            
-            String[] li = lines[i].split(" ");
-            arr[i] = makeTime(li[1],li[2]);
+            String[] temp = lines[i].split(" ");
+            arr[i] = convert(temp[1],temp[2]);
         }
         
-        int max = 0;
+        
         for(int i=0;i<arr.length;i++){
-            int cnt = 0;
-            for(int j=i;j<arr.length;j++){   
-                if(arr[i][1]+1000>arr[j][0]){
-                    cnt++;
+            int temp = 1;
+            int last = arr[i][1]+1000;
+            for(int j=i+1;j<arr.length;j++){
+                if(last>arr[j][0]){
+                    temp++;   
                 }
             }
-            max = Math.max(max,cnt);
+            answer = Math.max(temp,answer);
         }
         
-        return max;
+        return answer;
     }
     
-    private int[] makeTime(String time, String len){
-            
-        int[] ans = new int[2];
-        int hour = Integer.parseInt(time.substring(0,2));
-        int minute = Integer.parseInt(time.substring(3,5));
-        int second = Integer.parseInt(time.substring(6,8));
-        int se = Integer.parseInt(time.substring(9,time.length()));
-        ans[1] = hour*3600000 + minute * 60000 + second * 1000 + se;
-
-        len = len.replace("s","");
-        if(len.contains(".")){
-            String[] li = len.split("\\.");
-            int second2 = Integer.parseInt(li[0]);
-            int se2 = Integer.parseInt(li[1]);
-            if(li[1].length()<=1) se2*=100;
-            else if(li[1].length()<=2) se2*=10;
-            ans[0] = Integer.max(0,ans[1] - (second2*1000+se2) + 1);
-        }else{
-            ans[0] = Integer.max(0,ans[1]-Integer.parseInt(len)*1000 + 1);
-        }
-        return ans;
-
+    private int[] convert(String start, String len){
+        
+        String[] start_arr = start.split(":");
+        int start_time = Integer.parseInt(start_arr[0])*60*60*1000
+            + Integer.parseInt(start_arr[1])*60*1000
+            + (int)(Double.parseDouble(start_arr[2])*1000);
+        int len_time = (int)(Double.parseDouble(len.replace("s",""))*1000);
+        
+        return new int[]{start_time-len_time+1,start_time};
     }
     
-
 }
