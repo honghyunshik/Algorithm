a53a06288cb27f4ee82a45bab3e7a00577340af7
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-15 17:23:46 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 45.65 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,52 +1,46 @@
 import java.util.*;
 
-
 class Solution {
-
-    public static int start=0,end=Integer.MAX_VALUE;
-
     public int[] solution(String[] gems) {
-
-        int[] answer = new int[2];
-        HashSet<String> set = new HashSet<>();      //set 개수 파악용
-        for(String s:gems){
-            set.add(s);
-        }
-
-        int len = set.size();
-        int l=0,r=0;
-
-        HashSet<String> check = new HashSet<>();
-        HashMap<String,Integer> map = new HashMap<>();
-        while(l<gems.length&&r<gems.length){
-
         
-            while(check.size()<len&&r<gems.length){
-                map.put(gems[r],map.getOrDefault(gems[r],0)+1);
-                check.add(gems[r]);
-                r++;
-            }
-            if(check.size()==len){
-
-
-                String val = gems[l];   //삭제될 값
-                while(l<gems.length&&check.size()==len){
-                    map.put(gems[l],map.get(gems[l])-1);
-                    if(map.get(gems[l])==0) check.remove(gems[l]);
-                    l++;
-                }
-                if(end-start>r-l){
-
-                    start=l;
-                    end=r;
-                }
-            }
-
+        
+        HashMap<String,Integer> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        for(int i=0;i<gems.length;i++){ 
+            set.add(gems[i]);
         }
-        answer[0] = start;
-        answer[1] = end;
+        int type = set.size();
+        int start = 0;
+        int[] answer = new int[]{0,Integer.MAX_VALUE};
+        for(int i=0;i<gems.length;i++){
+            
+            String gem = gems[i];
+            int cnt = map.getOrDefault(gem,0);
+            map.put(gem,++cnt);
 
+            
+            if(cnt==1){
+                if(--type==0){
+                    for(int j=start;j<i;j++){
+                        cnt = map.get(gems[j]);
+                        map.put(gems[j],cnt-1);
+                        
+                        if(cnt==1) {
+                            if(answer[1]-answer[0]>i-j){
+                                answer[1] = i+1;
+                                answer[0] = j+1;
+                            }
+                            type++;
+                            start = j+1;
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+            
+        }
+        if(answer[0]==0&&answer[1]==Integer.MAX_VALUE) return new int[]{1,1};
         return answer;
     }
-
 }
