1ef943320e85b0501382562d803b3828aa2f41f5
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-15 15:00:43 +0900

    [level 3] Title: N으로 표현, Time: 15.27 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -1,40 +1,53 @@
 import java.util.*;
+    
 class Solution {
     public int solution(int N, int number) {
+        
         int answer = 0;
+        
+        int num = N;
         ArrayList<HashSet<Integer>> list = new ArrayList<>();
+        
         for(int i=0;i<=8;i++){
             list.add(new HashSet<>());
         }
         
-        list.get(1).add(N);
-        list.get(2).add(11*N);
-        list.get(3).add(111*N);
-        list.get(4).add(1111*N);
-        list.get(5).add(11111*N);
+        for(int i=1;i<=5;i++){
+            list.get(i).add(num);
+            num = num*10 + N;
+        }
         
         for(int i=2;i<=8;i++){
             
-            HashSet<Integer> set = list.get(i);
-            for(int j=1;j<=i;j++){
+            for(int j=1;j<i;j++){
                 
                 HashSet<Integer> first = list.get(j);
                 HashSet<Integer> second = list.get(i-j);
+                
                 for(int fi:first){
+                    
                     for(int se:second){
-                        set.add(fi+se);
-                        set.add(fi-se);
-                        set.add(fi*se);
-                        if(fi!=0&&se!=0) set.add(fi/se);
+                        
+                        
+                        list.get(i).add(fi+se);
+                        list.get(i).add(fi*se);
+                        if(fi>se) list.get(i).add(fi-se);
+                        if(fi>=se) list.get(i).add(fi/se);
                     }
                 }
             }
         }
         for(int i=1;i<=8;i++){
-            HashSet<Integer> set = list.get(i);
-            if(set.contains(number)) return i;
+            if(list.get(i).contains(number)) return i;
         }
         
+       
         return -1;
+    }   
+    
+    private boolean valid(int n){
+        
+        if(n>0&&n<100000) return true;
+        return false;
     }
 }
