b3be77b71f0f2677d0913088a6f4c6ccd6413b3f
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-16 16:57:37 +0900

    [level 3] Title: 합승 택시 요금, Time: 2.87 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,66 +1,65 @@
 import java.util.*;
+
 class Solution {
-    //n : 노드 수  s : 출발 지점   a : A 도착지점   b : B 도착지점
     
-    static ArrayList<ArrayList<int[]>> map;
-    static int N;
-    
+    int N;
+    ArrayList<ArrayList<int[]>> LIST = new ArrayList<>();
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        
-        N = n;
-        //map에 대한 설정
-        map = new ArrayList<>(); 
-        for(int i=0;i<=n;i++){
-            map.add(new ArrayList<>());
-        }
-        for(int i=0;i<fares.length;i++){
-            int l = fares[i][0];
-            int r = fares[i][1];
-            int pay = fares[i][2];
-            map.get(l).add(new int[]{r,pay});
-            map.get(r).add(new int[]{l,pay});
-        }
-        
-        int[] startS = dijkstra(s);
-        int[] backA = dijkstra(a);
-        int[] backB = dijkstra(b);
         int answer = Integer.MAX_VALUE;
-        for(int i=1;i<=n;i++){
-            answer = Math.min(answer,startS[i]+backA[i]+backB[i]);
+        N = n;
+        for(int i=0;i<=n;i++){
+            LIST.add(new ArrayList<>());
+        }
+        for(int[] fare:fares){
+            
+            int l = fare[0];
+            int r = fare[1];
+            int cost = fare[2];
+            LIST.get(l).add(new int[]{r,cost});
+            LIST.get(r).add(new int[]{l,cost});
         }
         
+        int[] with = dijkstra(s);
+        int[] justA = dijkstra(a);
+        int[] justB = dijkstra(b);
+        for(int i=1;i<=n;i++){
+            
+            answer = Math.min(answer, with[i] + justA[i] + justB[i]);
+            
+        }
         return answer;
-        
     }
-    private int[] dijkstra(int s){
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        pq.add(new int[]{s,0});
+    
+  
+    
+    private int[] dijkstra(int a){
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p,q)->p[1]-q[1]);
+        pq.add(new int[]{a,0});
         int[] dis = new int[N+1];
         int MAX = 9999999;
         Arrays.fill(dis,MAX);
-        dis[s] = 0;     //출발지 설정
-        
+        dis[a] = 0;     //출발지 설정
+
         while(!pq.isEmpty()){
-            
+
             int[] nowL = pq.poll();
             int now = nowL[0];
             int dist = nowL[1];
-            
-            for(int i=0;i<map.get(now).size();i++){
-                
-                int[] nextL = map.get(now).get(i);
+
+            for(int i=0;i<LIST.get(now).size();i++){
+
+                int[] nextL = LIST.get(now).get(i);
                 int next=  nextL[0];
                 int nextDis = nextL[1];
-                
+
                 if(dis[next]>dist+nextDis){
                     dis[next] = dist + nextDis;
                     pq.add(new int[]{next,dis[next]});
                 }
             }
         }
-        
+
         return dis;
     }
-    
 }
