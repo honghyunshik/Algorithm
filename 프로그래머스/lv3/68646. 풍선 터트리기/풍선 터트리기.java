197f64c09720c6a9868878b8d86ef81f66255dc9
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-16 15:53:37 +0900

    [level 3] Title: 풍선 터트리기, Time: 27.14 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,22 +1,29 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] a) {
-        int answer = a.length;
+        int answer = 2;
+        int[] leftMin = new int[a.length];
+        int[] rightMin = new int[a.length];
         
-        int leftMin = Integer.MAX_VALUE;
-        
-        int[] minList = new int[a.length];
-        
-        int rightMin = Integer.MAX_VALUE;
-        for(int i=a.length-1;i>=0;i--){
-            rightMin = Math.min(a[i],rightMin);
-            minList[i] = rightMin;
+        int min = a[0];
+        leftMin[0] = min;
+        for(int i=1;i<a.length;i++){
+            
+            min = Math.min(a[i],min);
+            leftMin[i] = min;
         }
+        
+        min = a[a.length-1];
+        rightMin[a.length-1] = min;
+        for(int i=a.length-2;i>=0;i--){
+            
+            min = Math.min(a[i],min);
+            rightMin[i] = min;
+        }
+        
         for(int i=1;i<a.length-1;i++){
-            leftMin = Math.min(leftMin,a[i-1]);
-            rightMin = minList[i+1];
-            if(a[i]>leftMin&&a[i]>rightMin) answer--;
+            
+            if(leftMin[i-1]>a[i]||rightMin[i+1]>a[i]) answer++;
         }
         return answer;
     }
