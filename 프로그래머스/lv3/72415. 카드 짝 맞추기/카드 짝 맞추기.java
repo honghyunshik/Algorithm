dcb092f36f8dca7302c3e9b89b158ca8883dba80
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-17 17:04:36 +0900

    [level 3] Title: 카드 짝 맞추기, Time: 2.96 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv3/72415. 카드 짝 맞추기/카드 짝 맞추기.java
+++ 프로그래머스/lv3/72415. 카드 짝 맞추기/카드 짝 맞추기.java
@@ -2,135 +2,99 @@ import java.util.*;
 
 class Solution {
     
-    int[][] D = {{1,0},{-1,0},{0,1},{0,-1}};
     HashMap<Integer,ArrayList<int[]>> map = new HashMap<>();
+    boolean[] VISITED;
     int[][] BOARD;
-    boolean[] visited;
-    int N,M;
-    int cnt = 0;
     int answer = Integer.MAX_VALUE;
-    
     public int solution(int[][] board, int r, int c) {
         
-        N = board.length;
-        M = board[0].length;
+        
         BOARD = board;
-        for(int i=0;i<N;i++){
-            
-            for(int j=0;j<M;j++){
-                
-                if(board[i][j]!=0){
-                    cnt++;
-                    ArrayList<int[]> list = map.get(board[i][j]);
-                    if(list==null){
-                        list = new ArrayList<>();
-                    }
-                    list.add(new int[]{i,j});
-                    map.put(board[i][j],list);
-                }
+        int max = 0;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                if(board[i][j]==0) continue;
+                max = Math.max(max,board[i][j]);
+                ArrayList<int[]> list = map.getOrDefault(board[i][j],new ArrayList<>());
+                list.add(new int[]{i,j});
+                map.put(board[i][j],list);
             }
         }
-         visited = new boolean[cnt/2+1];
-       System.out.println(getDis(3,2,0,0));
-        for(int i=1;i<=cnt/2;i++){
-            
-            visited[i] = true;
-            dfs(i,r,c,0);
-            visited[i] = false;
-            
+        VISITED = new boolean[max+1];
+        for(int i=1;i<=max;i++){
+            VISITED[i] = true;
+            dfs(r,c,i,0);
+            VISITED[i] = false;
         }
-       return answer;
+        
+        
+       
+        
+        
+        return answer;
     }
     
-    private void dfs(int num, int x, int y, int dis){
+    void dfs(int x, int y, int num, int dis){
         
-      // System.out.println("num : " + num + " x : " + x + " y : " + y + 
-        //                   " dis : "  + dis + " vis : " + Arrays.toString(visited));
         ArrayList<int[]> list = map.get(num);
-        int x1 = list.get(0)[0];
-        int y1 = list.get(0)[1];
-        int x2 = list.get(1)[0];
-        int y2 = list.get(1)[1];
         
-        int x1Tox2 = getDis(x,y,x1,y1) + getDis(x1,y1,x2,y2) + 2;
-        int x2Tox1 = getDis(x,y,x2,y2) + getDis(x2,y2,x1,y1) + 2;
+        int leftX = list.get(0)[0];
+        int leftY = list.get(0)[1];
+        int rightX = list.get(1)[0];
+        int rightY = list.get(1)[1];
         
-        boolean flag = true;
-        for(int i=1;i<=cnt/2;i++){
+        int lTor = getDis(x,y,leftX,leftY) + getDis(leftX,leftY,rightX,rightY) + 2;
+        int rTol = getDis(x,y,rightX,rightY) + getDis(rightX,rightY,leftX,leftY) + 2;
+        
+    
+  
+        
+        boolean flag = false;
+        for(int i=1;i<VISITED.length;i++){
             
-            if(!visited[i]){
-                flag = false;
-                visited[i] = true;
-                dfs(i,x1,y1,dis+x2Tox1);
-                dfs(i,x2,y2,dis+x1Tox2);
-                visited[i] = false;
+            if(i!=num&&!VISITED[i]){
+                flag = true;
+                VISITED[i] = true;
+                dfs(leftX,leftY,i,dis + rTol);
+                dfs(rightX,rightY,i,dis + lTor);
+                VISITED[i] = false;
             }
         }
-        if(flag){
-            
-            answer = Math.min(answer,dis + Math.min(x2Tox1,x1Tox2));
-            return;
-            
+        if(!flag){
+            answer = Math.min(answer,dis + Math.min(lTor,rTol));
         }
-        
     }
     
-    private int getDis(int x1, int y1, int x2, int y2){
+    int getDis(int x, int y, int X, int Y){
         
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(x1,y1,0));
-        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y, 0});
+        int[][] d = {{0,1},{0,-1},{1,0},{-1,0}};
         while(!queue.isEmpty()){
             
-            
-            Node now = queue.poll();
-             if(now.x==x2&&now.y==y2) return now.dis;
+            int[] now = queue.poll();
+            int nowX = now[0];
+            int nowY = now[1];
+            int nowDis = now[2];
+            if(nowX==X&&nowY==Y) return nowDis;
             
             for(int i=0;i<4;i++){
                 
-                int nextX = now.x + D[i][0];
-                int nextY = now.y + D[i][1];
-                int moreX = nextX;
-                int moreY = nextY;
-                
-                if(!stop(nextX,nextY)) {
-                    
-                    queue.add(new Node(nextX,nextY,now.dis+1));
-                    
-                    while(!stop(moreX+D[i][0],moreY+D[i][1])&&!(BOARD[moreX][moreY]!=0&&!visited[BOARD[moreX][moreY]])
-                         &&(moreX!=x2||moreY!=y2)){
-                        moreX += D[i][0];
-                        moreY += D[i][1];
-                    }
-
-                    if(moreX!=nextX||moreY!=nextY){
-                        queue.add(new Node(moreX,moreY,now.dis+1));
-                    }
-                    
-                }
-                    
-                
-                
-            }
+                int nextX = nowX + d[i][0];
+                int nextY = nowY + d[i][1];
+                if(nextX<0||nextX>3||nextY<0||nextY>3) continue;
+                queue.add(new int[]{nextX,nextY,nowDis+1});
               
+                while(nextX+d[i][0]>=0&&nextX+d[i][0]<=3&&nextY+d[i][1]>=0&&nextY+d[i][1]<=3&&(BOARD[nextX][nextY]==0||VISITED[BOARD[nextX][nextY]])&&(nextX!=X||nextY!=Y)){
+                    nextX += d[i][0];
+                    nextY += d[i][1];
+                }
+                
+                 if(nowX+d[i][0]!=nextX||nowY+d[i][1]!=nextY) queue.add(new int[]{nextX,nextY,nowDis+1});
+              
+            }
         }
-      
-        return -1;
-    }
-    
-    private boolean stop(int x, int y){
         
-        if(x<0||x>N-1||y<0||y>M-1) return true;
-        return false;
-    }
-}
-
-class Node{
-    
-    int x, y, dis;
-    Node(int x, int y, int dis){
-        this.x = x;
-        this.y = y;
-        this.dis = dis;
+        return -1;
     }
 }
