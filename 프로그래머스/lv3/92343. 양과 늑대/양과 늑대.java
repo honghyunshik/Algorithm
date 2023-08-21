a414bfe064af744569e85cfe61386660e5db4265
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-21 17:15:58 +0900

    [level 3] Title: 양과 늑대, Time: 1.48 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
@@ -1,48 +1,48 @@
 import java.util.*;
+
 class Solution {
-    
-    static ArrayList<ArrayList<Integer>> list;
-    static int answer = Integer.MIN_VALUE;
+    int answer = 0;
+    int[] INFO;
+    ArrayList<ArrayList<Integer>> EDGES = new ArrayList<>();
     public int solution(int[] info, int[][] edges) {
         
-        
-        list = new ArrayList<>();
+        INFO = info;
         for(int i=0;i<info.length;i++){
-            list.add(new ArrayList<>());
+            EDGES.add(new ArrayList<>());
         }
-        
-        for(int i=0;i<edges.length;i++){
-            list.get(edges[i][0]).add(edges[i][1]);
-            list.get(edges[i][1]).add(edges[i][0]);
+        for(int[] edge:edges){
+            int l = edge[0];
+            int r = edge[1];
+            EDGES.get(l).add(r);
+   
         }
-        int size = info.length;
-        boolean[][][] visited = new boolean[size][size+1][size+1];
-        dfs(0,0,0,info,visited);
-        
+        boolean[] visited = new boolean[info.length];
+        visited[0] = true;
+        dfs(0,0,0,new ArrayList<>(),visited);
         return answer;
     }
     
-    void dfs(int idx,int sheep, int wolf, int[] info, boolean[][][] visited){
+    void dfs(int idx, int sheep, int wolf, ArrayList<Integer> list, boolean[] visited){
         
-        if(info[idx]==0) sheep++;
-        else if(info[idx]==1) wolf++;
-        
-        if(wolf>=sheep) return;
+        if(INFO[idx]==0) sheep++;
+        else wolf++;
+        if(sheep<=wolf) return;
         answer = Math.max(answer,sheep);
         
-        for(int i=0;i<list.get(idx).size();i++){
-            
-            if(!visited[idx][sheep][wolf]){
-                visited[idx][sheep][wolf] = true;
-                int temp = info[idx];
-                int nextIdx = list.get(idx).get(i);
-                info[idx] = -1;
-                dfs(nextIdx,sheep,wolf,info,visited);
-                info[idx] = temp;
-                visited[idx][sheep][wolf] = false;
-            }
-            
-        }
         
+        ArrayList<Integer> child = EDGES.get(idx);
+        ArrayList<Integer> newList = new ArrayList<>();
+        newList.addAll(child);
+        newList.addAll(list);
+         newList.remove(Integer.valueOf(idx));
+        for(int i=0;i<newList.size();i++){
+            int next = newList.get(i);
+            if(next!=idx&&!visited[next]){
+                visited[next] = true;
+                dfs(next,sheep,wolf,newList,visited);
+                visited[next] = false;
+            }
+           
+        }
     }
 }
