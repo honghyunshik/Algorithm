93abc81ab14c65d4017584b9621327c5bb343a86
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-21 14:28:24 +0900

    [level 3] Title: 금과 은 운반하기, Time: 0.14 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/lv3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -1,44 +1,35 @@
-import java.util.*;
 class Solution {
     public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        
-        long l = 0;
-        //최대 걸리는 시간은 (a+b)/(용량/걸리는시간*2)
-        long r = (long)Math.pow(10,14)*4;
-        
-        //upper bound
+        long answer = -1;
+        long l = 0, r = (long)Math.pow(10,14)*4;
         while(l<r){
             
             long mid = (l+r)/2;
-            int gold = 0;
-            int silver = 0;
-            int plus = 0;
             
-            for(int i=0;i<g.length;i++){
-                
-                int nowGold = g[i];
-                int nowSilver = s[i];
-                int nowWeight = w[i];
-                int nowTime = t[i];
-                
-                long cnt = mid/nowTime/2;
-                if(mid%(nowTime*2)>=t[i]) cnt++;
-                
-                gold += Math.min(nowGold,nowWeight*cnt);
-                silver += Math.min(nowSilver,nowWeight*cnt);
-                plus += Math.min(nowGold+nowSilver,nowWeight*cnt);
-
-            }
-          
-            if(a<=gold&&b<=silver&&a+b<=plus){
-                r = mid;
-            }else{
+            if(!can(a,b,g,s,w,t,mid)){
                 l = mid+1;
-            }
-            
+            }else r = mid;
+        }
+        return l;
+    }
+    boolean can(int a, int b, int[] g, int [] s, int[] w, int[] t, long time){
+        
+        long sumG = 0;
+        long sumS = 0;
+        long sumAll = 0;
+        
+        for(int i=0;i<g.length;i++){
+               
+            long cnt = time%(t[i]*2)>=t[i]?time/t[i]/2+1:time/t[i]/2;
+            long tempG = Math.min(w[i] * cnt,g[i]);
+            long tempS = Math.min(w[i] * cnt,s[i]);
+            sumG += tempG;
+            sumS += tempS;
+            sumAll += Math.min(g[i]+s[i],w[i]*cnt);
             
         }
+        if(a<=sumG&&b<=sumS&&sumAll>=a+b) return true;
+        return false;
         
-        return r;
     }
 }
