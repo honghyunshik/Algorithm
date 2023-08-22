10af1c17ef0c400695ff767d4b40c924e83f1f98
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-22 21:18:20 +0900

    [level 3] Title: 카운트 다운, Time: 0.08 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/lv3/131129. 카운트 다운/카운트 다운.java
@@ -1,50 +1,33 @@
 import java.util.*;
+
 class Solution {
-    
-    //
     public int[] solution(int target) {
-        
+        int[][] dp = new int[Math.max(61,target+1)][2];
         int MAX = 9999999;
-        //0은 val   2는 싱글+볼
-        int[][] list = new int[Math.max(61,target+1)][2];
-        for(int i=1;i<list.length;i++){
-            list[i][0] = MAX;
+        for(int i=1;i<dp.length;i++){
+            dp[i][0] = MAX;
         }
         for(int i=1;i<=20;i++){
-            list[i][0] = 1;
-            list[i][1] = 1;
-            list[i*2][0] = 1;
-            list[i*2][1] = 0;
-            list[i*3][0] = 1;
-            list[i*3][1] = 0;
+            dp[i][0] = 1;
+            dp[i][1] = 1;
+            dp[i*2][0] = 1;
+            dp[i*3][0] = 1;
         }
+        dp[50][0] = 1;
+        dp[50][1] = 1;
         
-        list[50][0] = 1;
-        list[50][1] = 1;
-        
-        for(int num=21;num<=target;num++){
+        for(int i=21;i<=target;i++){
             
-            int min = list[num][0];
-            int cnt = list[num][1];
-            
-            for(int i=Math.max(1,num-60);i<num;i++){
-               
-                int j = num-i;
-                int tempValSum = list[i][0] + list[j][0];
-                int tempCntSum = list[i][1] + list[j][1];
-                if(min>tempValSum||
-                   min==tempValSum&&cnt<tempCntSum){  
-                    min = tempValSum;
-                    cnt = tempCntSum;
-                }           
+            for(int j=Math.max(1,i/2-60);j<=i/2;j++){
+                
+                if(dp[i][0]>dp[j][0] + dp[i-j][0]||(dp[i][0]==dp[j][0]+dp[i-j][0]&&dp[i][1]<dp[j][1]+dp[i-j][1])){
+                    dp[i][0] = dp[j][0] + dp[i-j][0];
+                    dp[i][1] = dp[j][1] + dp[i-j][1];
+                }
             }
-            list[num][0] = min;
-            list[num][1] = cnt;
-            
-            //list[num] = new int[]{min,cnt};
         }
+ 
         
-        
-        return list[target];
+        return new int[]{dp[target][0],dp[target][1]};
     }
 }
