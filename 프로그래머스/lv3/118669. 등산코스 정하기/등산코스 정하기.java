8cf1eddca954ddaad996ef1f18c2b90c4f124b9e
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-22 19:29:38 +0900

    [level 3] Title: 등산코스 정하기, Time: 126.77 ms, Memory: 163 MB -BaekjoonHub

--- 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,69 +1,73 @@
 import java.util.*;
+
 class Solution {
     
-    //gates는 출입구    summits는 산봉우리
-    //산봉우리 낮은걸로
+    
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
         
-        ArrayList<ArrayList<int[]>> list = new ArrayList<>();
+        int[] mount = new int[n+1];
+        ArrayList<ArrayList<int[]>> EDGE = new ArrayList<>();
+        Arrays.sort(summits);
         for(int i=0;i<=n;i++){
-            list.add(new ArrayList<>());
+            EDGE.add(new ArrayList<>());
         }
-        for(int i=0;i<paths.length;i++){
-            int l = paths[i][0];
-            int r = paths[i][1];
-            int dis = paths[i][2];
-            list.get(l).add(new int[]{r,dis});
-            list.get(r).add(new int[]{l,dis});
+        
+        for(int[] path:paths){
+            EDGE.get(path[0]).add(new int[]{path[1],path[2]});
+            EDGE.get(path[1]).add(new int[]{path[0],path[2]});
         }
-        HashMap<Integer,Integer> gates_map = new HashMap<>();
-        HashMap<Integer,Integer> summits_map = new HashMap<>();
+        
+        //출입구는 1, 산봉우리들은 2
+        
+        for(int num:gates){
+            mount[num] = 1;
+        }
+        for(int num:summits){
+            mount[num] = 2;
+        }
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b)->a.dis-b.dis);
+        
+        int[] dis = new int[n+1];
+        int summit = 0;
+        int intensity = Integer.MAX_VALUE;
+        Arrays.fill(dis,Integer.MAX_VALUE);
         for(int gate:gates){
-            gates_map.put(gate,1);
+            pq.add(new Node(gate,0));
+            dis[gate] = 0;
         }
-        for(int summit:summits){
-            summits_map.put(summit,1);
-        }
-        int min_intensity = Integer.MAX_VALUE;
-        int san = min_intensity;
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b)->a.intensity-b.intensity);
-        for(int gate:gates){
-            boolean[] visited = new boolean[n+1];
-            for(int i=0;i<list.get(gate).size();i++){
-                int[] arr = list.get(gate).get(i);
-                pq.add(new Node(arr[0],arr[1]));
+        while(!pq.isEmpty()){
+            Node node = pq.poll();
+            int nowIdx = node.idx;
+            int nowDis = node.dis;
+            if(nowDis>dis[nowIdx]) continue;
+            if(mount[nowIdx]==2) continue;
+            ArrayList<int[]> edge = EDGE.get(nowIdx);
+            for(int[] ed:edge){
+                int next = ed[0];
+                int nextDis = ed[1];
+                if(mount[next]==1) continue;
+                if(dis[next]>Math.max(nextDis,nowDis)){
+                    dis[next] = Math.max(nextDis,nowDis);
+                    pq.add(new Node(next,dis[next]));
+                }
             }
-            while(!pq.isEmpty()){
-                Node now = pq.poll();
-                if(now.intensity>min_intensity) continue;   //최소가 아니면 pass
-                if(summits_map.get(now.num)!=null){     //산봉우리에 도착했으면 비교
-                    if((min_intensity==now.intensity&&san>now.num)||min_intensity>now.intensity){
-                        san = now.num;
-                        min_intensity = now.intensity; 
-                    }
-                    continue;
-                }
-                for(int i=0;i<list.get(now.num).size();i++){
-                    int[] arr = list.get(now.num).get(i);
-                    if(arr[1]>min_intensity) continue;
-                    if(gates_map.get(arr[0])!=null) continue;  //출발지는 pass
-                    if(visited[arr[0]]) continue;      //이미 방문한 node면 pass
-                    visited[now.num] = true;
-                    pq.add(new Node(arr[0],Math.max(arr[1],now.intensity)));         
-                }
-            }   
         }
-        return new int[]{san,min_intensity};
-    }
-}
+        for(int sum:summits){
 
-class Node{
-    int num; 
-    int intensity;      //최대 이동시간
-    
-    Node(int num, int intensity){
-        this.num = num;
-        this.intensity = intensity;
+            if(dis[sum]<intensity){
+                intensity = dis[sum];
+                summit = sum;
+            }
+        }
+        return new int[]{summit,intensity};
     }
-           
+ 
+}
+class Node{
+    int idx,dis;
+    Node(int idx, int dis){
+        this.idx = idx;
+        this.dis = dis;
+    }
 }
