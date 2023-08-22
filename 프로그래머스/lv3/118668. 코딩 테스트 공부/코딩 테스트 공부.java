6e6782010e09ad7c9c57c01abc4ef80931cb7216
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-22 17:20:27 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 1.35 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,50 +1,39 @@
-
 import java.util.*;
+
 class Solution {
+    int[][] dp;
+    int N=0,M=0;
     public int solution(int alp, int cop, int[][] problems) {
-        
         int answer = 0;
-        int maxAlp = Integer.MIN_VALUE;
-        int maxCop = Integer.MIN_VALUE;
+        int MAX = 999999;
         
-        for(int i=0;i<problems.length;i++){
-            maxAlp = Math.max(problems[i][0],maxAlp);
-            maxCop = Math.max(problems[i][1],maxCop);
+        for(int[] problem:problems){
+            N = Math.max(N,problem[0]);
+            M = Math.max(M,problem[1]);
         }
-        if(maxAlp<=alp&&maxCop<=cop) return 0;
-        alp = Math.min(alp,maxAlp);
-        cop = Math.min(cop,maxCop);
-        int[][] dp = new int[maxAlp+2][maxCop+2];
-        
-        for(int i=alp;i<=maxAlp;i++){
-            for(int j=cop;j<=maxCop;j++){
-                dp[i][j] = Integer.MAX_VALUE;
-            }
+        if(alp>=N&&cop>=M) return 0;
+        alp = Math.min(alp,N);
+        cop = Math.min(cop,M);
+        dp = new int[N+1][M+1];
+        for(int i=alp;i<=N;i++){
+            Arrays.fill(dp[i],MAX);
         }
-        
         dp[alp][cop] = 0;
-        
-        for(int i=alp;i<=maxAlp;i++){
-            for(int j=cop;j<=maxCop;j++){
+        for(int i=alp;i<=N;i++){
+            
+            for(int j=cop;j<=M;j++){
                 
-                //알고리즘 공부
-                dp[i+1][j] = Math.min(dp[i][j]+1,dp[i+1][j]);
-                //코딩력 공부
-                dp[i][j+1] = Math.min(dp[i][j]+1,dp[i][j+1]);
+                dp[Math.min(i+1,N)][j] = Math.min(dp[Math.min(i+1,N)][j],dp[i][j]+1);
+                dp[i][Math.min(j+1,M)] = Math.min(dp[i][Math.min(j+1,M)],dp[i][j]+1);
                 
                 for(int[] problem:problems){
-                    //문제를 풀 수 있을 경우
-                    if(problem[0]<=i&&problem[1]<=j){
-                        int nextI = Math.min(i + problem[2],maxAlp);
-                        int nextJ = Math.min(j + problem[3],maxCop);
-                        dp[nextI][nextJ] = Math.min(dp[nextI][nextJ],dp[i][j]+problem[4]);
-                    } 
-                } 
+                    
+                    if(i>=problem[0]&&j>=problem[1]){
+                        dp[Math.min(i+problem[2],N)][Math.min(j+problem[3],M)] = Math.min(dp[Math.min(i+problem[2],N)][Math.min(j+problem[3],M)], dp[i][j] + problem[4]);
+                    }
+                }
             }
         }
-        
-        
-        
-        return dp[maxAlp][maxCop];
+        return dp[N][M];
     }
 }
