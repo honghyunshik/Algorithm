68929077ecd23d18e44fe6fff1447318f110f359
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-24 21:23:00 +0900

    [unrated] Title: 억억단을 외우자, Time: 721.17 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138475. 억억단을 외우자/억억단을 외우자.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int e, int[] starts) {
+        
+        int[] list = new int[e+1];
+        yaksu(list);
+        int[] answer = new int[starts.length];
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<starts.length;i++){
+            map.put(starts[i],i);
+        }
+        Arrays.sort(starts);
+        int idx = 0;
+        int max = 0;
+        for(int i=0;i<starts.length;i++){
+            if(idx<starts[i]){
+                idx = 0;
+                max = 0;
+                for(int j=starts[i];j<=e;j++){
+                    if(max<list[j]){
+                        idx = j;
+                        max = list[j];
+                    }
+                }
+            }
+            answer[map.get(starts[i])] = idx;
+        }
+        
+        return answer;
+    }
+    
+    public void yaksu(int[] list){
+        int e = list.length-1;
+        for(int i=1;i<=e;i++){
+            for(int j=1;j<=e/i;j++){
+                list[i*j]++;
+            }
+        }
+        
+    }
+}
