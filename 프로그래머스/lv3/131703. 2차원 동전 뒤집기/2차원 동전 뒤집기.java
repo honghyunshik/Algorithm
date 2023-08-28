925c6f638f67bf885358ab19b02664b4bf3ccc2c
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-28 22:39:56 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
+++ 프로그래머스/lv3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -1,66 +1,75 @@
 import java.util.*;
 
 class Solution {
+    
     int[][] TARGET;
-    int[][] temp;
     int N,M;
     int answer = Integer.MAX_VALUE;
+    
     public int solution(int[][] beginning, int[][] target) {
         
-        N = beginning.length;
-        M = beginning[0].length;
+        N = target.length;
+        M = target[0].length;
         TARGET = new int[N][M];
-        temp = new int[N][M];
+        
         
         for(int i=0;i<N;i++){
             for(int j=0;j<M;j++){
-                if(beginning[i][j]==target[i][j]) TARGET[i][j] = 0;
-                else TARGET[i][j] = 1;
+                if(beginning[i][j]!=target[i][j]) TARGET[i][j] = 1;
             }
         }
-        
-        force(-1,0);
+       
+        dfs(0,new int[N],0);
         if(answer==Integer.MAX_VALUE) return -1;
-        
-        
         return answer;
     }
     
-    private void force(int rowIdx,  int cnt){
-           
-        int check = check();
-        if(check>=0){
-            answer = Math.min(answer,cnt + check);
+    
+    
+    void dfs(int idx, int[] row, int cnt){
+        
+        if(idx==N){
+            
+            int temp = fit(row);
+            
+            if(temp!=-1){
+                answer = Math.min(answer,temp+cnt); 
+            }
             return;
         }
         
         
-        for(int i=rowIdx+1;i<M;i++){
-            
-            for(int j=0;j<N;j++){
-                temp[j][i]++;
-            }
-            force(i,cnt+1);
-            for(int j=0;j<N;j++){
-                temp[j][i]++;
-            }
-        }
-        
+        dfs(idx+1,row,cnt);
+        row[idx]++;
+        dfs(idx+1,row,cnt+1);
+        row[idx]--;
     }
     
-    private int check(){
+    int fit(int[] row){
         
-        boolean flag = true;
         int ans = 0;
+        int[] col = new int[M];
+        for(int i=0;i<M;i++){
+            if(TARGET[0][i]!=row[0]) {
+                col[i] = 1;
+                ans++;
+            }
+        }
+        
+        
         for(int i=0;i<N;i++){
-            boolean tempFlag = (temp[i][0]%2==TARGET[i][0]);
             
             for(int j=0;j<M;j++){
-                if(tempFlag!=(temp[i][j]%2==TARGET[i][j])) return -1;
+                
+                if((row[i]+col[j])%2!=TARGET[i][j]) {
+                    
+                    return -1;
+                }
+                
             }
-            if(temp[i][0]%2!=TARGET[i][0]) ans++;
         }
+       
         return ans;
+        
     }
 }
-
