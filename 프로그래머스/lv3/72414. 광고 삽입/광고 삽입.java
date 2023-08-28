4625d83488b1aa7500fd17faee9b2169604f80bf
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-28 18:43:20 +0900

    [level 3] Title: 광고 삽입, Time: 8.72 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/lv3/72414. 광고 삽입/광고 삽입.java
@@ -1,69 +1,64 @@
-import java.util.*;
-
 class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
         
-        int play = strToInt(play_time);     //총 상영시간
-        int ad = strToInt(adv_time);        //총 광고시간
-        //빨리 끝나는 순서대로 정렬
-        long[] cntList = new long[play+1];
-        for(int i=0;i<logs.length;i++){
-            String[] log = logs[i].split("-");
-            int start = strToInt(log[0]);
-            int end = strToInt(log[1]);
-            for(int j=start;j<end;j++){
-                cntList[j]++;
+        String answer = "";
+        int play = toInt(play_time);
+        int adv = toInt(adv_time);
+        int[] tempSum = new int[play+1];
+        for(String log:logs){
+            
+            String[] temp = log.split("-");
+            int start = toInt(temp[0]);
+            int end = toInt(temp[1]);
+            tempSum[start]++;
+            tempSum[end]--;
+        }
+        
+        for(int i=1;i<tempSum.length;i++){
+            
+            tempSum[i] += tempSum[i-1];
+        }
+        
+        long total = 0;
+        int maxTime = 0;
+        for(int i=0;i<adv;i++){
+            total += tempSum[i];
+        }
+        long maxTotal = total;
+        for(int i=0;i<play-adv;i++){
+            
+            total += tempSum[i+adv]-tempSum[i];
+            if(total>maxTotal){
+                maxTotal = total;
+                maxTime = i+1;
             }
         }
         
         
-        for(int i=0;i<play;i++){
-            cntList[i+1]+=cntList[i];
-        }
         
-        long max = cntList[ad-1];
-        long sum = 0;
-        int start = 0;
-        
-        //i는 start고 i+ad가 end
-        for(int i=0;i<=play-ad;i++){
-
-            long temp = cntList[i+ad]-cntList[i]; 
-            if(max<temp){
-                max = temp;
-                start = i+1;
-            }
-        }
-
-        String answer = intToStr(start);
-        
-        return answer;
+        return toStr(maxTime);
     }
     
-    private String intToStr(int num){
+    String toStr(int num){
         
         int hour = num/3600;
-        num %= 3600;
+        num%=3600;
         int minute = num/60;
-        num %= 60;
+        num%=60;
         int second = num;
-        String str = "";
-        if(hour<10) str+="0";
-        str+=hour + ":";
-        if(minute<10) str+="0";
-        str+=minute + ":";
-        if(second<10) str+="0";
-        str+=second;
-        return str;
+        StringBuilder str = new StringBuilder();
+        if(hour<10) str.append("0");
+        str.append(hour + ":");
+        if(minute<10) str.append("0");
+        str.append(minute + ":");
+        if(second<10) str.append("0");
+        str.append(second);
+        return str.toString();
     }
     
-    
-    private int strToInt(String str){
+    int toInt(String str){
         
-        String[] p = str.split(":");
-        int hour = Integer.parseInt(p[0]);
-        int minute = Integer.parseInt(p[1]);
-        int second = Integer.parseInt(p[2]);
-        return hour*3600+minute*60+second;
+        String[] temp = str.split(":");
+        return Integer.parseInt(temp[0])*60*60 + Integer.parseInt(temp[1])*60 + Integer.parseInt(temp[2]); 
     }
 }
