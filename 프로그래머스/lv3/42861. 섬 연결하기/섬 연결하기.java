658e37be9a2d698d079bd6937dde769220bb37c1
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-08-29 18:07:46 +0900

    [level 3] Title: 섬 연결하기, Time: 0.59 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,26 +1,29 @@
 import java.util.*;
+
 class Solution {
-    //다익스트라는 한점에서 다른 점들로 가는 최소거리를 찾는 알고리즘이다
     
     public int solution(int n, int[][] costs) {
-        int answer = 0;
+        
         Arrays.sort(costs,(a,b)->a[2]-b[2]);
         boolean[] visited = new boolean[n];
         visited[costs[0][0]] = true;
         visited[costs[0][1]] = true;
-        answer += costs[0][2];
-        int k = 2;
-        while(k<n){
+        int answer = costs[0][2];
+        int cnt = 2;
+        while(cnt<n){
             for(int i=1;i<costs.length;i++){
-                if(visited[costs[i][0]]^visited[costs[i][1]]){
-                    visited[costs[i][0]] = true;
-                    visited[costs[i][1]] = true;
-                    k++;
-                    answer+=costs[i][2];
+                int l = costs[i][0];
+                int r = costs[i][1];
+                if(visited[l]!=visited[r]){
+                    visited[l] = true;
+                    visited[r] = true;
+                    answer += costs[i][2];
+                    cnt++;
                     break;
                 }
             }
         }
+        
         
         return answer;
     }
