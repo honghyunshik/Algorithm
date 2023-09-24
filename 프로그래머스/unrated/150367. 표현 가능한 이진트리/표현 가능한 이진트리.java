b44610444314bbf8d825d9ac6d7b74ad66a3e179
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-09-24 16:44:08 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 30.89 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,65 +1,87 @@
 import java.util.*;
 class Solution {
-
-    static boolean check;
     public int[] solution(long[] numbers) {
+        
         int[] answer = new int[numbers.length];
-        int idx = 0;
-        for(long number:numbers){
-            if(number==1){
-                answer[idx++] = 1;
-                continue;
-            }
-            check = true;
-            String binary = toBinary(number);
-            char[] list = new char[binary.length()+1];
-            for(int i=1;i<list.length;i++){
-                list[i] = binary.charAt(i-1);
-            }
-            int rootIdx = list.length/2;
-            dfs(rootIdx,rootIdx,list);
-            if(check) answer[idx++] = 1;
-            else answer[idx++] = 0;
-            //System.out.println("\n\n-----\n\n");
+        
+         for(int idx=0;idx<numbers.length;idx++){
+            
+            char[] arr = toBinary(numbers[idx]).toCharArray();
+            
+            char result = dfs(arr.length/2,getDepth(arr.length/2),arr);
+            if(result=='x') answer[idx] = 0;
+            else answer[idx] = 1;
+            
         }
+        
         return answer;
     }
     
-    private void dfs(int parentIdx, int temp, char[] list){
+    //높이 구하기
+    int getDepth(int num){
         
-        temp/=2;
-        if(temp==0||!check) return;
-        
-        int leftChildIdx = parentIdx-temp;
-        int rightChildIdx = parentIdx+temp;
-        //System.out.println(parentIdx + " " + leftChildIdx + " " + rightChildIdx + " " + temp);
-        if(list[parentIdx]==48&&(list[leftChildIdx]==49||list[rightChildIdx]==49)){
-            check = false;
-            return;
+        int dep = 1;
+        while(num>1){
+            dep++;
+            num/=2;
         }
-        dfs(leftChildIdx,temp,list);
-        dfs(rightChildIdx,temp,list);
+        return dep;
     }
-
-
-    private String toBinary(long num){
-        StringBuilder sb = new StringBuilder(Long.toBinaryString(num));
-        long j = 1;
-        while(Math.pow(2,j)<=num){
-            j = j*2+1;
+    
+    //표현할 수 없는 경우 -> 자식 노드는 1인데 부모 노드가 0인 경우
+    char dfs(int idx, int depth, char[] arr){
+        
+        //idx가 홀수이다 -> 리프노드이다
+        if(idx%2!=0||depth==1){
+            return arr[idx];
         }
-        long cnt = j-sb.length();
-        while(cnt>0){
-            sb.insert(0,0);
-            cnt--;
+        int leftChildIdx = idx - (int)Math.pow(2,depth-2);
+        int rightChildIdx = idx + (int)Math.pow(2,depth-2);
+      
+        char leftVal = dfs(leftChildIdx,depth-1,arr);
+        char rightVal = dfs(rightChildIdx,depth-1,arr);
+        
+        //자식노드에서 X면 부모노드도 X
+        if(leftVal=='x'||rightVal=='x') return 'x';
+        //자식노드가 1인데 부모노드가 0이면 X
+        if(arr[idx]=='0'&&(leftVal=='1'||rightVal=='1')) return 'x';
+        
+        return arr[idx];
+    }
+    
+    int getLen(long num){
+        
+        //길이 1 : 1 ~ 2^1 - 1
+        //길이 3 : 2^1 ~ 2^3-1
+        //길이 7 : 2^3 ~ 2^7-1
+        if(num==1) return 1;
+        
+        int k = 0;
+        
+        while(num>Math.pow(2,Math.pow(2,k)-1)-1){
+            k++;
+        }
+        
+        return (int)Math.pow(2,k)-1;
+     }
+    
+    //앞에 더미노드를 추가한 문자열 생성
+    //앞에 0하나 더붙여서 1부터 Index 시작
+    String toBinary(long num){
+        
+        StringBuilder sb = new StringBuilder();
+        int len = getLen(num);
+        while(num>0){
+            sb.insert(0,num%2);
+            num/=2;
+        }
+        
+        int cnt = len-sb.length();
+        
+        for(int i=0;i<=cnt;i++){
+            sb.insert(0,"0");
         }
         return sb.toString();
-    }
-}
-class Node{
-    int data;
-    Node left,right;
-    Node(int data){
-        this.data = data;
+        
     }
 }
