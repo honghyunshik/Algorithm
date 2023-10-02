4e4be343da1a41fd1d5f347b124ef3d07d0f2490
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-10-02 19:10:20 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 44.29 ms, Memory: 219 MB -BaekjoonHub

--- 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,49 +1,53 @@
+import java.util.*;
+
 class Solution {
     
     int[][] BOARD;
     int N,M;
-    
     public int solution(int[][] board, int[][] skill) {
-        
         int answer = 0;
         N = board.length;
         M = board[0].length;
         BOARD = new int[N+1][M+1];
-        
         for(int[] sk:skill){
-            int startL = sk[1];
-            int startR = sk[2];
-            int endL = sk[3];
-            int endR = sk[4];
-            int num = sk[0]==1?-sk[5]:sk[5];
-            BOARD[startL][startR] += num;
-            BOARD[startL][endR+1] -= num;
-            BOARD[endL+1][startR] -= num;
-            BOARD[endL+1][endR+1] += num;
-        }
-        //상하 합
-        for(int i=1;i<=N;i++){
             
-            for(int j=0;j<=M;j++){
-                
-                BOARD[i][j] += BOARD[i-1][j];
-            }
+            int r1 = sk[1];
+            int c1 = sk[2];
+            int r2 = sk[3];
+            int c2 = sk[4];
+            int degree = sk[5];
+            if(sk[0]==1) degree = -degree;
+            BOARD[r1][c1] += degree;
+            BOARD[r1][c2+1] -= degree;
+            BOARD[r2+1][c1] -= degree;
+            BOARD[r2+1][c2+1] += degree;
+            
         }
         
         //좌우 합
-        for(int i=1;i<=M;i++){
+        for(int i=0;i<N;i++){
             
-            for(int j=0;j<=N;j++){
+            for(int j=1;j<M;j++){
                 
-                BOARD[j][i] += BOARD[j][i-1];
+                BOARD[i][j] += BOARD[i][j-1];
+            }
+        }    
+        
+        //상하 합
+        for(int i=0;i<M;i++){
+            
+            for(int j=1;j<N;j++){
+                
+                BOARD[j][i] += BOARD[j-1][i];
             }
         }
+        
         
         for(int i=0;i<N;i++){
             
             for(int j=0;j<M;j++){
                 
-                if(BOARD[i][j] + board[i][j]>0) answer++;
+                if(BOARD[i][j] + board[i][j] >0) answer ++;
             }
         }
         return answer;
