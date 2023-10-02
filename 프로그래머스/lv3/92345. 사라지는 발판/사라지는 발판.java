0714a15ce2ad18ec5ac060f7efd2016ccab6b9b0
honghyunshik <93652895+honghyunshik@users.noreply.github.com>
2023-10-02 19:33:40 +0900

    [level 3] Title: 사라지는 발판, Time: 0.28 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/lv3/92345. 사라지는 발판/사라지는 발판.java
@@ -2,58 +2,63 @@ class Solution {
     
     int[][] BOARD;
     int[][] D = {{0,1},{0,-1},{1,0},{-1,0}};
-    int N, M;
-    
+    int N,M;
     public int solution(int[][] board, int[] aloc, int[] bloc) {
-        
+        int answer = -1;
         BOARD = board;
         N = board.length;
         M = board[0].length;
         
-        return dfs(aloc[0],aloc[1],bloc[0],bloc[1],0).cnt;
+        return dfs(aloc[0],aloc[1],bloc[0],bloc[1]).dis;
     }
     
-    Result dfs(int aL,int aR, int bL, int bR, int dis){
+    Result dfs(int x1, int y1, int x2, int y2){
         
+        //내 발판이 사라졌다면 패배
+        if(BOARD[x1][y1]==0) return new Result(false,0);
         
         boolean win = false;
-        int max = 0, min = Integer.MAX_VALUE;
-        if(BOARD[aL][aR]==0) return new Result(false,0);
-        
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        BOARD[x1][y1] = 0;
         for(int i=0;i<4;i++){
             
-            int nextL = aL + D[i][0];
-            int nextR = aR + D[i][1];
-            if(!can(nextL,nextR)) continue;
-            BOARD[aL][aR] = 0;
-            Result result = dfs(bL,bR,nextL,nextR,dis+1);
-            BOARD[aL][aR] = 1;
-            if(!result.win){
+            int nextX1 = x1 + D[i][0];
+            int nextY1 = y1 + D[i][1];
+            if(!valid(nextX1,nextY1)) continue;
+            Result result = dfs(x2,y2,nextX1,nextY1);
+            //이길 수 있다면 최대한 짧게
+            if(!result.win) {
                 win = true;
-                min = Math.min(min,result.cnt);
-            }else max = Math.max(max,result.cnt);
+                min = Math.min(result.dis,min);    
+            }
+            else max = Math.max(result.dis,max);
+            
         }
-        
+        BOARD[x1][y1] = 1;
+        //상하좌우 다 이동 불가능 -> 패배
         if(max==0&&min==Integer.MAX_VALUE){
-            return new Result(false,0);
+            return new Result(false,0);   
         }
+   
         return new Result(win,win?min+1:max+1);
     }
     
-    boolean can(int x, int y){
-        
+    boolean valid(int x, int y){
+
+        //보드 밖인 경우
         if(x<0||x>N-1||y<0||y>M-1) return false;
+        //발판이 없으면 안됨
         if(BOARD[x][y]==0) return false;
         return true;
     }
 }
 
 class Result{
-    
     boolean win;
-    int cnt;
-    Result(boolean win, int cnt){
+    int dis;
+    Result(boolean win, int dis){
         this.win = win;
-        this.cnt = cnt;
+        this.dis = dis;
     }
 }
